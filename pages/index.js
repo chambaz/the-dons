6ade49ccf389f3746c940cb917cbc044d0cce2b8
Adam Chambers <adammchambers@gmail.com>
2022-04-12 11:12:18 -0400

    FPO bg image on home page

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,9 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-screen h-screen text-xl bg-center bg-cover bg-dons-white font-dons-altona">
+      <div
+        className="w-screen h-screen text-xl bg-center bg-cover bg-dons-white font-dons-altona"
+        style={{ backgroundImage: 'url(/img/bg-fpo.jpg)' }}>
         <Nav />
       </div>
     </>
