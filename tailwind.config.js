749047739348dbbcf94db7c39146c94ebcf830c7
Adam Chambers <adammchambers@gmail.com>
2022-04-11 23:23:11 -0400

    Add Dons theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dons-black': '#282e22',
+        'dons-white': '#ebebeb',
+        'dons-cyan': '#8fe6f1',
+        'dons-red': '#e27458',
+      },
+    },
   },
   plugins: [],
 }
